28f972b0f461a6f399196b06b6c58797df35e91f
MiHi1224 <kawaguchi.mihiro0810@gmail.com>
2024-12-18 10:26:42 +0900

    12/18

--- base.js
+++ base.js
@@ -1,7 +1,7 @@
 const menu = document.getElementById('js-menu');
 const nav = document.getElementById('js-nav');
 
-
+/* ハンバーガーメニュー切り替え   */
 menu.addEventListener("click", () => {
     nav.classList.toggle('nav--active');
 });
